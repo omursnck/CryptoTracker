6bf29a28ebfe44b534566c9ff1c96203560f57aa
omursnck <omrsnr12@icloud.com>
2023-04-12 03:00:16 +0300

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Ömür Şenocak on 12.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
